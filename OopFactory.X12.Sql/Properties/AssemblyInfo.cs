04c44d007ccca391086829ffda66584fc0683b1c
Bradley Powers <bradley.powers@wth.org>
2023-05-23 08:46:26 -0500

    Correct validation job schedule

--- OopFactory.X12.Sql/Properties/AssemblyInfo.cs
+++ OopFactory.X12.Sql/Properties/AssemblyInfo.cs
@@ -35,6 +35,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2023.5.22.2207")]
-[assembly: AssemblyFileVersion("2023.5.22.2207")]
-[assembly: AssemblyInformationalVersion("2023.5.22.2207")]
+[assembly: AssemblyVersion("2023.5.23.1346")]
+[assembly: AssemblyFileVersion("2023.5.23.1346")]
+[assembly: AssemblyInformationalVersion("2023.5.23.1346")]
