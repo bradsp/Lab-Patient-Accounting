2238a84c0d3fa0c372093811dc78986219642117
Bradley Powers <bradley.powers@wth.org>
2023-05-25 17:03:17 -0500

    Setup announcement board to pull from wiki page.

--- Lab Billing UI/Forms/DashboardForm.Designer.cs
+++ Lab Billing UI/Forms/DashboardForm.Designer.cs
@@ -29,13 +29,12 @@
         private void InitializeComponent()
         {
             this.dashboardLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.arChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.announcementLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
             this.dashboardLayoutPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.arChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dashboardLayoutPanel
@@ -43,9 +42,8 @@
             this.dashboardLayoutPanel.ColumnCount = 3;
             this.dashboardLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.331F));
             this.dashboardLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.669F));
-            this.dashboardLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 138F));
+            this.dashboardLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 139F));
             this.dashboardLayoutPanel.Controls.Add(this.arChart, 0, 0);
-            this.dashboardLayoutPanel.Controls.Add(this.tableLayoutPanel1, 1, 1);
             this.dashboardLayoutPanel.Controls.Add(this.pictureBox1, 2, 0);
             this.dashboardLayoutPanel.Controls.Add(this.announcementLayoutPanel, 0, 1);
             this.dashboardLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -57,16 +55,6 @@
             this.dashboardLayoutPanel.Size = new System.Drawing.Size(997, 490);
             this.dashboardLayoutPanel.TabIndex = 0;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::LabBilling.Properties.Resources.logoicon2;
-            this.pictureBox1.Location = new System.Drawing.Point(861, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
             // arChart
             // 
             this.arChart.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -76,18 +64,15 @@
             this.arChart.TabIndex = 1;
             this.arChart.Text = "chart1";
             // 
-            // tableLayoutPanel1
+            // pictureBox1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.72589F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.27411F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(452, 248);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34.22819F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 65.77181F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(394, 149);
-            this.tableLayoutPanel1.TabIndex = 3;
+            this.pictureBox1.Image = global::LabBilling.Properties.Resources.logoicon2;
+            this.pictureBox1.Location = new System.Drawing.Point(861, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // announcementLayoutPanel
             // 
@@ -113,8 +98,8 @@
             this.Load += new System.EventHandler(this.DashboardForm_Load);
             this.dashboardLayoutPanel.ResumeLayout(false);
             this.dashboardLayoutPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.arChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -124,7 +109,6 @@
         private System.Windows.Forms.TableLayoutPanel dashboardLayoutPanel;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.DataVisualization.Charting.Chart arChart;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TableLayoutPanel announcementLayoutPanel;
     }
 }
