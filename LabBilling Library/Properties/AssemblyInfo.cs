ef783ee33f389ca42a4886295ca71b07d596fd61
Bradley Powers <bradspowers@outlook.com>
2023-12-19 16:21:25 -0600

    Corrected Collections State/Zip issue.

--- LabBilling Library/Properties/AssemblyInfo.cs
+++ LabBilling Library/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2023.1.4.597")]
-[assembly: AssemblyFileVersion("2023.1.4.597")]
+[assembly: AssemblyVersion("2023.1.4.598")]
+[assembly: AssemblyFileVersion("2023.1.4.598")]
 
-[assembly: AssemblyInformationalVersion("2023.1.4.597")]
+[assembly: AssemblyInformationalVersion("2023.1.4.598")]
