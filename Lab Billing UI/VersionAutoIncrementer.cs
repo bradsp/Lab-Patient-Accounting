aa52aa67b199e5ca174f8b4e75899acd8f280376
Bradley Powers <bradspowers@outlook.com>
2024-04-20 16:18:28 -0500

    Setup project fix

--- Lab Billing UI/VersionAutoIncrementer.cs
+++ Lab Billing UI/VersionAutoIncrementer.cs
@@ -6,9 +6,9 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab Patient Accounting")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright 2020-2024")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("...")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2024.4.19.989")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2024.4.19.989")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2024.4.20.968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2024.4.20.968")]
 [assembly: System.Reflection.AssemblyProductAttribute("...")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab Patient Accounting")]
-[assembly: AssemblyVersion("2024.4.19.989")]
+[assembly: AssemblyVersion("2024.4.20.968")]
 
