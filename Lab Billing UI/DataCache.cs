b5a70f9fdfe329c588bf99b4b4620c515332dc31
Bradley Powers <bradspowers@outlook.com>
2024-05-09 16:32:16 -0500

    Corrected issue with error message during client change when client fee schedule was not populated.

--- Lab Billing UI/DataCache.cs
+++ Lab Billing UI/DataCache.cs
@@ -1,128 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using LabBilling.Core.DataAccess;
-using LabBilling.Core.Models;
+﻿using LabBilling.Core.Models;
 using LabBilling.Core.UnitOfWork;
 using LazyCache;
 
-namespace LabBilling
+namespace LabBilling;
+
+public sealed class DataCache
 {
-    public sealed class DataCache
+    IAppCache cache = new CachingService();
+    private static readonly UnitOfWorkMain unitOfWork = new(Program.AppEnvironment);
+
+    private DataCache()
     {
-        IAppCache cache = new CachingService();
-        private static readonly UnitOfWorkMain unitOfWork = new(Program.AppEnvironment);
 
-        private DataCache() 
+    }
+
+    private static DataCache instance = null;
+
+    public static DataCache Instance
+    {
+        get
         {
-            
-        }
-
-        private static DataCache instance = null;
-
-        public static DataCache Instance
-        {
-            get
+            if (instance == null)
             {
-                if(instance == null)
-                {
-                    instance = new DataCache();
-                }
-                return instance;
+                instance = new DataCache();
             }
-        }
-
-        readonly Func<IEnumerable<Fin>> finGetter = () => unitOfWork.FinRepository.GetActive();
-        
-        public List<Fin> GetFins()
-        {
-            var fins = cache.GetOrAdd("fin", finGetter);
-
-            return fins.ToList();
-        }
-
-        public void ClearFinCache()
-        {
-            cache.Remove("fin");
-        }
-
-        readonly Func<IEnumerable<InsCompany>> inscGetter = () => unitOfWork.InsCompanyRepository.GetAll();
-
-        public List<InsCompany> GetInsCompanies()
-        {
-            var inscs = cache.GetOrAdd("insc", inscGetter);
-
-            return inscs.ToList();
-        }
-
-        public void ClearInscCache()
-        {
-            cache.Remove("insc");
-        }
-
-        readonly Func<IEnumerable<Phy>> phyGetter = () => unitOfWork.PhyRepository.GetActive();
-
-        public List<Phy> GetProviders()
-        {
-            var phys = cache.GetOrAdd("phy", phyGetter);
-
-            return phys.ToList();
-        }
-
-        public void ClearProviderCache()
-        {
-            cache.Remove("phy");
-        }
-
-        readonly Func<IEnumerable<Client>> clientGetter = () => unitOfWork.ClientRepository.GetAll(false);
-
-        public List<Client> GetClients()
-        {
-            var clients = cache.GetOrAdd("client", clientGetter);
-
-            return clients.ToList();
-        }
-
-        readonly Func<IEnumerable<Client>> clientAllGetter = () => unitOfWork.ClientRepository.GetAll(true);
-
-        public List<Client> GetClientsIncludeInactive()
-        {
-            var clients = cache.GetOrAdd("client", clientAllGetter);
-            return clients.ToList();
-        }
-
-        public void ClearClientCache()
-        {
-            cache.Remove("client");
-        }
-
-        readonly Func<IEnumerable<Cdm>> cdmGetter = () => unitOfWork.CdmRepository.GetAll(false);
-
-        public List<Cdm> GetCdms()
-        {
-            var cdms = cache.GetOrAdd("cdm", cdmGetter);
-
-            return cdms.ToList();
-        }
-
-        public void ClearCdmCache()
-        {
-            cache.Remove("cdm");
-        }
-
-        readonly Func<IEnumerable<RevenueCode>> revenueCodeGetter = () => unitOfWork.RevenueCodeRepository.GetAll();
-
-        public List<RevenueCode> GetRevenueCodes()
-        {
-            var revenuecodes = cache.GetOrAdd("revcode", revenueCodeGetter);
-
-            return revenuecodes.ToList();
-        }
-
-        public void ClearRevenueCodeCache()
-        {
-            cache.Remove("revcode");
+            return instance;
         }
     }
 
+    readonly Func<IEnumerable<Fin>> finGetter = () => unitOfWork.FinRepository.GetActive();
+
+    public List<Fin> GetFins()
+    {
+        var fins = cache.GetOrAdd("fin", finGetter);
+
+        return fins.ToList();
+    }
+
+    public void ClearFinCache()
+    {
+        cache.Remove("fin");
+    }
+
+    readonly Func<IEnumerable<InsCompany>> inscGetter = () => unitOfWork.InsCompanyRepository.GetAll();
+
+    public List<InsCompany> GetInsCompanies()
+    {
+        var inscs = cache.GetOrAdd("insc", inscGetter);
+
+        return inscs.ToList();
+    }
+
+    public void ClearInscCache()
+    {
+        cache.Remove("insc");
+    }
+
+    readonly Func<IEnumerable<Phy>> phyGetter = () => unitOfWork.PhyRepository.GetActive();
+
+    public List<Phy> GetProviders()
+    {
+        var phys = cache.GetOrAdd("phy", phyGetter);
+
+        return phys.ToList();
+    }
+
+    public void ClearProviderCache()
+    {
+        cache.Remove("phy");
+    }
+
+    readonly Func<IEnumerable<Client>> clientGetter = () => unitOfWork.ClientRepository.GetAll(false);
+
+    public List<Client> GetClients()
+    {
+        var clients = cache.GetOrAdd("client", clientGetter);
+
+        return clients.ToList();
+    }
+
+    readonly Func<IEnumerable<Client>> clientAllGetter = () => unitOfWork.ClientRepository.GetAll(true);
+
+    public List<Client> GetClientsIncludeInactive()
+    {
+        var clients = cache.GetOrAdd("client", clientAllGetter);
+        return clients.ToList();
+    }
+
+    public void ClearClientCache()
+    {
+        cache.Remove("client");
+    }
+
+    readonly Func<IEnumerable<Cdm>> cdmGetter = () => unitOfWork.CdmRepository.GetAll(false);
+
+    public List<Cdm> GetCdms()
+    {
+        var cdms = cache.GetOrAdd("cdm", cdmGetter);
+
+        return cdms.ToList();
+    }
+
+    public void ClearCdmCache()
+    {
+        cache.Remove("cdm");
+    }
+
+    readonly Func<IEnumerable<RevenueCode>> revenueCodeGetter = () => unitOfWork.RevenueCodeRepository.GetAll();
+
+    public List<RevenueCode> GetRevenueCodes()
+    {
+        var revenuecodes = cache.GetOrAdd("revcode", revenueCodeGetter);
+
+        return revenuecodes.ToList();
+    }
+
+    public void ClearRevenueCodeCache()
+    {
+        cache.Remove("revcode");
+    }
 }
--- Lab Billing UI/VersionAutoIncrementer.cs
+++ Lab Billing UI/VersionAutoIncrementer.cs
@@ -6,9 +6,9 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab Patient Accounting")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright 2020-2024")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("...")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2024.5.6.832")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2024.5.6.832")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2024.5.9.993")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2024.5.9.993")]
 [assembly: System.Reflection.AssemblyProductAttribute("...")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab Patient Accounting")]
-[assembly: AssemblyVersion("2024.5.6.832")]
+[assembly: AssemblyVersion("2024.5.9.993")]
 
